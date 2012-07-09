40c25b7233666990dfbe8c1571c9a6379fde97b2
pjaspers <junkiesxl@gmail.com>
2012-07-09 13:10:13 +0200

    Basic app setup.

--- Gemfile
+++ Gemfile
@@ -1,2 +1,5 @@
 source :rubygems
 gem "sinatra"
+
+gem "broach", "~> 0.2.1"
+gem "mail", "~> 2.4.4"
--- app.rb
+++ app.rb
@@ -1,16 +1,52 @@
 # -*- coding: utf-8 -*-
 require "rubygems"
 require 'sinatra'
+require "broach"
+require "mail"
 
 class App
 
   before do
+    Broach.settings = {
+      'account' => "10to1",
+      'token' => "dc906dcf93d6277d4b62276441ffd9c55c90b5b2",
+      'use_ssl' => true
+    }
   end
 
-  configure do
+  helpers do
+
+    def campfire_message(mail)
+      message = mail.subject.split("Re:").first.stript
+
+      if message =~ /Unsuccessful/
+        message = "Epic Fail: #{message}"
+      end
+
+      if message =~ /^Successful/
+        message = "Great Success: #{message}"
+      end
+
+      ":fax: #{message}"
+    end
+
+    def speak(message)
+      campfire_room.speak(message)
+    end
+
+    def campfire_room
+      Broach::Room.find_by_name("General")
+    end
+  end
+
+  post '/mail' do
+    return unless params[:message]
+
+    speak campfire_message(Mail.new(params[:message]))
+    status 200
   end
 
   get '/' do
-    erb :index
+    "Hummer catch catches Hubot's mail"
   end
 end
